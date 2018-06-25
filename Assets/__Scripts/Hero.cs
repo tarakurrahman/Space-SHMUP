ecbd26a82b0b75734f0099c9527edf28c3318865
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-06-24 23:37:23 -0400

    We are now able to shoot but they don't kill the enemy
    In the next step, we will script the projectile

--- Assets/__Scripts/Hero.cs
+++ Assets/__Scripts/Hero.cs
@@ -10,6 +10,8 @@ public class Hero : MonoBehaviour {
 	public float	rollMult = -45;
 	public float  	pitchMult=30;
 	public float gameRestartDelay = 2f;
+	public GameObject projectilePrefab;
+	public float projectileSpeed = 40;
 
     [Header("Set dynamically")]
 	[SerializeField]
@@ -37,6 +39,18 @@ public class Hero : MonoBehaviour {
 		
 		// rotate the ship to make it feel more dynamic
 		transform.rotation = Quaternion.Euler(yAxis*pitchMult, xAxis*rollMult, 0);
+
+		// Allow the ship to fire
+		if ( Input.GetKeyDown( KeyCode.Space ) ) { // a
+			TempFire();
+		}
+	}
+
+	void TempFire() { // b
+		GameObject projGO = Instantiate<GameObject>( projectilePrefab );
+		projGO.transform.position = transform.position;
+		Rigidbody rigidB = projGO.GetComponent<Rigidbody>();
+		rigidB.velocity = Vector3.up * projectileSpeed;
 	}
 
 	void OnTriggerEnter(Collider other) {
