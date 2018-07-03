5508e5b43d62e20fb67b8c9b2af72a2fa08ebde5
Tarakur Rahman <39719842+tarakurrahman@users.noreply.github.com>
2018-07-02 23:02:46 -0400

    Space-SHMUP+: The enemy turns red when hit
    Next step: Adding Power-Ups and Boosting Weapons

--- Assets/__Scripts/Enemy.cs
+++ Assets/__Scripts/Enemy.cs
@@ -8,15 +8,30 @@ public class Enemy : MonoBehaviour {
 	public float fireRate = 0.3f; // Seconds/shot (Unused)
 	public float health = 10;
 	public int score = 100; // Points earned for destroying this
+    public float showDamageDuration = 0.1f; // # seconds to show damage // a
 
-	protected BoundsCheck bndCheck;
+    [Header("Set Dynamically: Enemy")]
+    public Color[] originalColors;
+    public Material[] materials;// All the Materials of this & its children
+    public bool showingDamage = false;
+    public float damageDoneTime; // Time to stop showing damage
+    public bool notifiedOfDestruction = false; // Will be used later
 
-	void Awake() { // b
-		bndCheck = GetComponent<BoundsCheck>();
-	}
 
-	// This is a Property: A method that acts like a field
-	public Vector3 pos {
+    protected BoundsCheck bndCheck;
+
+    void Awake() {
+        bndCheck = GetComponent<BoundsCheck>();
+        // Get materials and colors for this GameObject and its children
+        materials = Utils.GetAllMaterials(gameObject); // b
+        originalColors = new Color[materials.Length];
+        for (int i = 0; i < materials.Length; i++) {
+            originalColors[i] = materials[i].color;
+        }
+    }
+
+    // This is a Property: A method that acts like a field
+    public Vector3 pos {
 		get {
 			return( this.transform.position );
 		}
@@ -25,15 +40,20 @@ public class Enemy : MonoBehaviour {
 		}
 	}
 
-	void Update() {
-		Move();
-		if ( bndCheck != null && bndCheck.offDown ) { // a
-			// We're off the bottom, so destroy this GameObject // b
-			Destroy( gameObject ); // b
-		}
-	}
+    void Update() {
+        Move();
 
-	public virtual void Move() { // b
+        if (showingDamage && Time.time > damageDoneTime) { // c
+            UnShowDamage();
+        }
+
+        if (bndCheck != null && bndCheck.offDown) {
+            // We're off the bottom, so destroy this GameObject
+            Destroy(gameObject);
+        }
+    }
+
+    public virtual void Move() { // b
 		Vector3 tempPos = pos;
 		tempPos.y -= speed * Time.deltaTime;
 		pos = tempPos;
@@ -51,6 +71,7 @@ public class Enemy : MonoBehaviour {
                 }
 
                 // Hurt this Enemy
+                ShowDamage();
                 // Get the damage amount from the Main WEAP_DICT.
                 health -= Main.GetWeaponDefinition(p.type).damageOnHit;
                 if (health <= 0) { // d
@@ -63,5 +84,20 @@ public class Enemy : MonoBehaviour {
                 print("Enemy hit by non-ProjectileHero: " + otherGO.name); // f
                 break;
         }
+    }
+
+    void ShowDamage() { // e
+        foreach (Material m in materials) {
+            m.color = Color.red;
+        }
+        showingDamage = true;
+        damageDoneTime = Time.time + showDamageDuration;
+    }
+
+    void UnShowDamage() { // f
+        for (int i = 0; i < materials.Length; i++) {
+            materials[i].color = originalColors[i];
+        }
+        showingDamage = false;
     }
 }
